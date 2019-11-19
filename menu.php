fab52d7da9bf7624d4d7faa62b20bde22f8aec07
Bri <b.therville@gmail.com>
2019-11-19 12:12:36 +0100

    new menu

--- menu.php
+++ menu.php
@@ -7,23 +7,23 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="accueil.php">Accueil <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Articles</a>
+              <a class="nav-link" href="articles.php">Articles</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Connexion</a>
+              <a class="nav-link" href="login.php">Connexion</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Mon compte</a>
+              <a class="nav-link" href="user_infos.php">Mon compte</a>
             </li>
             <li class="nav-item" >
-              <a class="nav-link" href="#">Créer un compte</a>
+              <a class="nav-link" href="create_user.php">Créer un compte</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item" style="position: absolute; right: 0;">
                 <button type="button" class="btn btn-default btn my-2 my-sm-0" >
-                    <span class="glyphicon glyphicon-log-out"></span> Déconnexion
+                    <span class="glyphicon glyphicon-log-out"></span> <a href="deconnexion.php">Déconnexion</a>
                   </button>
             </li>
           </ul>
